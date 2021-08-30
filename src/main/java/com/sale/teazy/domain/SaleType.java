e745d11ef3f64d356d34e4e8728ecbfeae6ca92f
FeridQuluzade <feridquluzade2002@gmail.com>
2021-08-30 13:08:21 +0400

    saleType_domain_hotfix: add some fields for SaleType and make softDelete

--- src/main/java/com/sale/teazy/domain/SaleType.java
+++ src/main/java/com/sale/teazy/domain/SaleType.java
@@ -5,9 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -15,14 +19,18 @@ import javax.persistence.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@SQLDelete(sql = "UPDATE saleTypes SET deleted_at= now() WHERE id=?")
+@Where(clause = "deleted_at is null")
 public class SaleType {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
-
     private String saleType;
-
     private Double commission;
-
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 }
