e915211d612f6d1fc44be0899463927d27042a3b
FeridQuluzade <feridquluzade2002@gmail.com>
2021-08-30 10:45:19 +0400

    Liquibase: migrate Liquibase

--- src/main/java/com/sale/teazy/domain/SaleType.java
+++ src/main/java/com/sale/teazy/domain/SaleType.java
@@ -1,12 +1,4 @@
 package com.sale.teazy.domain;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
-public enum SaleType {
-    BOLT,WOLT,TEAZY;
-
-    @JsonValue
-    public String toLower() {
-        return this.toString().toLowerCase();
-    }
+public class SaleType {
 }
